1077d453eab988eae667126327ba65a07d6264a6
MilesWilliams <miles@klarity.co.za>
2016-09-26 12:30:35 +0200

    Implemented nos-child theme, and have added icons to the cars cpt

---
+++ wp-content/themes/Nos-child/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <title>Nos</title>
+    <?php  wp_head(); ?>
+
+  </head>
+
+  <body <?php body_class(); ?>>
