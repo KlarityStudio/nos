19f716f1a91cbe0f702ceab85cc721339fefdd1d
MilesWilliams <miles@klarity.co.za>
2016-10-11 11:44:16 +0200

    Removed fake migrate db

--- wp-content/themes/Nos/search.php
+++ wp-content/themes/Nos/search.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
-<h1>Search Page</h1>
-<h2><?php echo the_title(); ?></h2>
+<section>
+  <h1>Search Page</h1>
+  <?php echo do_shortcode('[searchandfilter fields="search,post_types,wpcm_make_model" post_types="wpcm_vehicle" taxonomies="wpcm_make_model" headings="Post Types"]') ?>
+  <h2><?php echo the_title(); ?></h2>
+  <div class="car-details">
+    <?php echo the_post_thumbnail(); ?>
+  </div>
+</section>
 <?php get_footer(); ?>
-<?php echo do_shortcode('[searchandfilter fields="search,post_types,wpcm_make_model" post_types="wpcm_vehicle" taxonomies="wpcm_make_model" headings="Post Types"]') ?>
